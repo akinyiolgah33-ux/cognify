90187f88366b65a33b49823a9f1c97485c73af91
akinyiolgah33-ux <akinyiolgah33@gmail.com>
2026-07-11 21:50:32 +0300

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,79 +1,59 @@
-import { ClientDuplexStream, ClientReadableStream, ClientUnaryCall, ClientWritableStream, ServiceError } from './call';
-import { CallCredentials, OAuth2Client } from './call-credentials';
-import { StatusObject } from './call-interface';
-import { Channel, ChannelImplementation } from './channel';
-import { CompressionAlgorithms } from './compression-algorithms';
-import { ConnectivityState } from './connectivity-state';
-import { ChannelCredentials, VerifyOptions } from './channel-credentials';
-import { CallOptions, Client, ClientOptions, CallInvocationTransformer, CallProperties, UnaryCallback } from './client';
-import { LogVerbosity, Status, Propagate } from './constants';
-import { Deserialize, loadPackageDefinition, makeClientConstructor, MethodDefinition, Serialize, ServerMethodDefinition, ServiceDefinition } from './make-client';
-import { Metadata, MetadataOptions, MetadataValue } from './metadata';
-import { ConnectionInjector, Server, ServerOptions, UntypedHandleCall, UntypedServiceImplementation } from './server';
-import { KeyCertPair, ServerCredentials } from './server-credentials';
-import { StatusBuilder } from './status-builder';
-import { handleBidiStreamingCall, handleServerStreamingCall, handleClientStreamingCall, handleUnaryCall, sendUnaryData, ServerUnaryCall, ServerReadableStream, ServerWritableStream, ServerDuplexStream, ServerErrorResponse } from './server-call';
-export { OAuth2Client };
-/**** Client Credentials ****/
-export declare const credentials: {
-    /**
-     * Combine a ChannelCredentials with any number of CallCredentials into a
-     * single ChannelCredentials object.
-     * @param channelCredentials The ChannelCredentials object.
-     * @param callCredentials Any number of CallCredentials objects.
-     * @return The resulting ChannelCredentials object.
-     */
-    combineChannelCredentials: (channelCredentials: ChannelCredentials, ...callCredentials: CallCredentials[]) => ChannelCredentials;
-    /**
-     * Combine any number of CallCredentials into a single CallCredentials
-     * object.
-     * @param first The first CallCredentials object.
-     * @param additional Any number of additional CallCredentials objects.
-     * @return The resulting CallCredentials object.
-     */
-    combineCallCredentials: (first: CallCredentials, ...additional: CallCredentials[]) => CallCredentials;
-    createInsecure: typeof ChannelCredentials.createInsecure;
-    createSsl: typeof ChannelCredentials.createSsl;
-    createFromSecureContext: typeof ChannelCredentials.createFromSecureContext;
-    createFromMetadataGenerator: typeof CallCredentials.createFromMetadataGenerator;
-    createFromGoogleCredential: typeof CallCredentials.createFromGoogleCredential;
-    createEmpty: typeof CallCredentials.createEmpty;
-};
-/**** Metadata ****/
-export { Metadata, MetadataOptions, MetadataValue };
-/**** Constants ****/
-export { LogVerbosity as logVerbosity, Status as status, ConnectivityState as connectivityState, Propagate as propagate, CompressionAlgorithms as compressionAlgorithms, };
-/**** Client ****/
-export { Client, ClientOptions, loadPackageDefinition, makeClientConstructor, makeClientConstructor as makeGenericClientConstructor, CallProperties, CallInvocationTransformer, ChannelImplementation as Channel, Channel as ChannelInterface, UnaryCallback as requestCallback, };
 /**
- * Close a Client object.
- * @param client The client to close.
+ * @module @opentelemetry/api
  */
-export declare const closeClient: (client: Client) => void;
-export declare const waitForClientReady: (client: Client, deadline: Date | number, callback: (error?: Error) => void) => void;
-export { sendUnaryData, ChannelCredentials, CallCredentials, Deadline, Serialize as serialize, Deserialize as deserialize, ClientUnaryCall, ClientReadableStream, ClientWritableStream, ClientDuplexStream, CallOptions, MethodDefinition, StatusObject, ServiceError, ServerUnaryCall, ServerReadableStream, ServerWritableStream, ServerDuplexStream, ServerErrorResponse, ServerMethodDefinition, ServiceDefinition, UntypedHandleCall, UntypedServiceImplementation, VerifyOptions, };
-/**** Server ****/
-export { handleBidiStreamingCall, handleServerStreamingCall, handleUnaryCall, handleClientStreamingCall, };
-export type Call = ClientUnaryCall | ClientReadableStream<any> | ClientWritableStream<any> | ClientDuplexStream<any, any>;
-/**** Unimplemented function stubs ****/
-export declare const loadObject: (value: any, options: any) => never;
-export declare const load: (filename: any, format: any, options: any) => never;
-export declare const setLogger: (logger: Partial<Console>) => void;
-export declare const setLogVerbosity: (verbosity: LogVerbosity) => void;
-export { ConnectionInjector, Server, ServerOptions };
-export { ServerCredentials };
-export { KeyCertPair };
-export declare const getClientChannel: (client: Client) => Channel;
-export { StatusBuilder };
-export { Listener, InterceptingListener } from './call-interface';
-export { Requester, ListenerBuilder, RequesterBuilder, Interceptor, InterceptorOptions, InterceptorProvider, InterceptingCall, InterceptorConfigurationError, NextCall, } from './client-interceptors';
-export { GrpcObject, ServiceClientConstructor, ProtobufTypeDefinition, } from './make-client';
-export { ChannelOptions } from './channel-options';
-export { getChannelzServiceDefinition, getChannelzHandlers } from './channelz';
-export { addAdminServicesToServer } from './admin';
-export { ServiceConfig, LoadBalancingConfig, MethodConfig, RetryPolicy, } from './service-config';
-export { ServerListener, FullServerListener, ServerListenerBuilder, Responder, FullResponder, ResponderBuilder, ServerInterceptingCallInterface, ServerInterceptingCall, ServerInterceptor, } from './server-interceptors';
-export { ServerMetricRecorder } from './orca';
-import * as experimental from './experimental';
-export { experimental };
-import { Deadline } from './deadline';
+export type { BaggageEntry, BaggageEntryMetadata, Baggage, } from './baggage/types';
+export { baggageEntryMetadataFromString } from './baggage/utils';
+export type { Exception } from './common/Exception';
+export type { HrTime, TimeInput } from './common/Time';
+export type { Attributes, AttributeValue } from './common/Attributes';
+export { createContextKey, ROOT_CONTEXT } from './context/context';
+export type { Context, ContextManager } from './context/types';
+export type { ContextAPI } from './api/context';
+export { DiagConsoleLogger } from './diag/consoleLogger';
+export { DiagLogLevel } from './diag/types';
+export type { DiagLogFunction, DiagLogger, ComponentLoggerOptions, DiagLoggerOptions, } from './diag/types';
+export type { DiagAPI } from './api/diag';
+export { createNoopMeter } from './metrics/NoopMeter';
+export type { MeterOptions, Meter } from './metrics/Meter';
+export type { MeterProvider } from './metrics/MeterProvider';
+export { ValueType } from './metrics/Metric';
+export type { Counter, Gauge, Histogram, MetricOptions, UpDownCounter, MetricAdvice, MetricAttributes, MetricAttributeValue, Observable, ObservableCounter, ObservableGauge, ObservableUpDownCounter, BatchObservableCallback, ObservableCallback, BatchObservableResult, ObservableResult, } from './metrics/Metric';
+export type { MetricsAPI } from './api/metrics';
+export { defaultTextMapGetter, defaultTextMapSetter, } from './propagation/TextMapPropagator';
+export type { TextMapPropagator, TextMapSetter, TextMapGetter, } from './propagation/TextMapPropagator';
+export type { PropagationAPI } from './api/propagation';
+export type { SpanAttributes, SpanAttributeValue } from './trace/attributes';
+export type { Link } from './trace/link';
+export { ProxyTracer, type TracerDelegator } from './trace/ProxyTracer';
+export { ProxyTracerProvider } from './trace/ProxyTracerProvider';
+export type { Sampler } from './trace/Sampler';
+export { SamplingDecision, type SamplingResult } from './trace/SamplingResult';
+export type { SpanContext } from './trace/span_context';
+export { SpanKind } from './trace/span_kind';
+export type { Span } from './trace/span';
+export type { SpanOptions } from './trace/SpanOptions';
+export { type SpanStatus, SpanStatusCode } from './trace/status';
+export { TraceFlags } from './trace/trace_flags';
+export type { TraceState } from './trace/trace_state';
+export { createTraceState } from './trace/internal/utils';
+export type { TracerProvider } from './trace/tracer_provider';
+export type { Tracer } from './trace/tracer';
+export type { TracerOptions } from './trace/tracer_options';
+export { isSpanContextValid, isValidTraceId, isValidSpanId, } from './trace/spancontext-utils';
+export { INVALID_SPANID, INVALID_TRACEID, INVALID_SPAN_CONTEXT, } from './trace/invalid-span-constants';
+export type { TraceAPI } from './api/trace';
+import { context } from './context-api';
+import { diag } from './diag-api';
+import { metrics } from './metrics-api';
+import { propagation } from './propagation-api';
+import { trace } from './trace-api';
+export { context, diag, metrics, propagation, trace };
+declare const _default: {
+    context: import("./api/context").ContextAPI;
+    diag: import("./api/diag").DiagAPI;
+    metrics: import("./api/metrics").MetricsAPI;
+    propagation: import("./api/propagation").PropagationAPI;
+    trace: import("./api/trace").TraceAPI;
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
